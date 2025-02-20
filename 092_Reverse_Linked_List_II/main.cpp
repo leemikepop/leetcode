fdd0e613bc370919c5c9b59112c766c9d13b3814
Robby <robbylee0929@gmail.com>
2025-02-20 16:52:34 +0800

    092_finished_20250220

--- 092_Reverse_Linked_List_II/main.cpp
+++ 092_Reverse_Linked_List_II/main.cpp
@@ -16,21 +16,38 @@ class Solution {
 public:
     ListNode* reverseBetween(ListNode* head, int left, int right) {
         int index = 1;
+        ListNode *dummy = new ListNode(0, head);
+        ListNode *preList = dummy;
+        
+        for(int i = 1; i < left; i++) {
+            preList = preList->next;
+        }
+
         ListNode *ReverseList = nullptr;
-        while(head) {
-            if (index == left-1) head->next = ReverseList;
-            else if (index >= left && index <= right) ReverseList = new ListNode(head->val, ReverseList);
-            else if (index == right+1) ReverseList->next = head;
+        while(index <= right) {
+            if (index >= left && index <= right) ReverseList = new ListNode(head->val, ReverseList);
             head = head->next;
             index++;
         }
+        
+        ListNode *current = preList;
+        preList->next = ReverseList;
+        while(current->next) {
+            current = current->next;
+        }
+        current->next = head;
+
+        ListNode *newHead = dummy->next;
+        delete dummy;
+        return newHead;
     }
 
     ListNode* createLinkedList(vector<int> v)  {
         ListNode* head = new ListNode(v[0]);
         ListNode* current = head;
+        ListNode *Node = head;
         
-        for(int i = 1; i <= v.size(); i++) {
+        for(int i = 1; i < v.size(); i++) {
             current->next = new ListNode(v[i]);
             current = current->next;
         }
@@ -52,6 +69,12 @@ int main(int argc, char *argv[]) {
         {1,2,3,4,5}
     };
     ListNode* head = sol->createLinkedList(test_cases[0]);
-    cout << sol->reverseBetween(head, 2, 4) << endl;
+    ListNode* ReverseList = nullptr;
+    ReverseList = sol->reverseBetween(head, 2, 4);
+    while(ReverseList) {
+        cout << ReverseList->val << " ";
+        ReverseList = ReverseList->next;
+    }
+    cout << endl;
     return 0;
 }
