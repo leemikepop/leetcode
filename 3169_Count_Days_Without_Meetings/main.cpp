91f99868c1f507dc1f2b9975ca98b57fefb43399
RobbyLi <93488148+Lesdabit@users.noreply.github.com>
2025-03-24 20:32:30 +0800

    3169_Add_Merging_Intervals_20250324 (#27)
    Co-authored-by: Lesdabit <robbylee0929@gmail.com>

--- 3169_Count_Days_Without_Meetings/main.cpp
+++ 3169_Count_Days_Without_Meetings/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
 
 using namespace std;
 
@@ -22,6 +23,29 @@ public:
 
         return days;
     }
+
+    int countDays_merge_intervals(int days, vector<vector<int>> meetings) {
+        vector<vector<int>> merged;
+        sort(meetings.begin(), meetings.end());
+
+        merged.push_back(meetings[0]);
+        for(int i = 1; i < meetings.size(); i++) {
+     
+            vector<int>& last = merged.back();
+
+            if(last[1] >= meetings[i][0]) {
+                last[1] = max(last[1], meetings[i][1]);
+            } else {
+                merged.push_back(meetings[i]);
+            }
+        }
+
+        for(auto& merge : merged) {
+            days -= merge[1]-merge[0]+1;
+        }
+
+        return days;
+    }
 };
 
 int main(int argc, char* argv[]) {
@@ -33,8 +57,14 @@ int main(int argc, char* argv[]) {
         {6, {{1,6}}}
     };
 
+    cout << "Solution 1:" << endl;
     for(auto& test_case : test_cases) {
         cout << solution.countDays(test_case.first, test_case.second) << endl;
+    }
+
+    cout << endl << "Solution 2 (Merging Intervals):" << endl;
+    for(auto& test_case : test_cases) {
+        cout << solution.countDays_merge_intervals(test_case.first, test_case.second) << endl;
     }
 
     return 0;
